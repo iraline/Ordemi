715637c3295280a624f1aa625cf334aacb1bdf66
iraline <iraline@hotmail.com>
2021-08-22 13:07:31 -0300

    removendo diretorio para iniciar um novo projeto

--- ordemi/src/main/java/com/example/ordemi/model/data/event/EventDAO.java
+++
@@ -1,7 +0,0 @@
-package com.example.ordemi.model.data.event;
-
-import com.example.ordemi.model.business.event.Event;
-import org.springframework.data.repository.CrudRepository;
-
-public interface EventDAO extends CrudRepository<Event, Long> {
-}
--- ordemi/src/main/java/com/example/ordemi/model/data/gymClass/GymClassDAO.java
+++
@@ -1,6 +0,0 @@
-package com.example.ordemi.model.data.gymClass;
-
-import com.example.ordemi.model.business.gymClass.GymClass;
-import org.springframework.data.repository.CrudRepository;
-
-public interface GymClassDAO extends CrudRepository<GymClass, Long> {}
